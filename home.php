9997fc6dc07b95f37bd92a5f16ea36776e6040bb
yurinastiti <yuriasiva@gmail.com>
2024-12-16 10:30:28 +0700

    first commit

---
+++ home.php
@@ -0,0 +1,24 @@
+<?php
+	include("session.php");
+	
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="css/mystyle.css" /> 
+
+</head>
+<body>
+
+<div class="icon-bar">
+  <a class="active" href="home.php"><i class="fa fa-home"></i></a> 
+  <a href="users.php"><i class="fa fa-user"></i></a> 
+  <a href="registration.php"><i class="fa fa-registered"></i></a>
+  <a href="print_all.php" target="_blank"><i class="fa fa-print"></i></a>
+  <a href="logout.php"><i class="fa fa-power-off"></i></a> 
+</div>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</body>
+</html> 
